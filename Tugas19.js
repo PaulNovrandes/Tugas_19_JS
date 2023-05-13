b27360339ca718f5323ae91114ba04db95884e09
PaulNovrandes <novrandes.tanuma@gmail.com>
2023-05-13 08:40:34 +0700

    Tugas 19 JS
    Membuat fungsi repeat. Tugas ini dikumpulkan untuk niomic.

---
+++ Tugas19.js
@@ -0,0 +1,23 @@
+function repeater1(){
+  var kata = "semangat..."
+
+  console.log(kata.repeat(5));
+}
+
+function repeater2(){
+  var b = "*";
+
+  console.log(b.repeat(5));
+  console.log(b.repeat(25));
+  console.log(b.repeat(40));
+  console.log(b.repeat(55));
+  console.log(b.repeat(70));
+  console.log(b.repeat(55));
+  console.log(b.repeat(40));
+  console.log(b.repeat(25));
+  console.log(b.repeat(5));
+}
+
+repeater1();
+console.log("");
+repeater2();
